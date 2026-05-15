887b5848948f3e87aa23c332e4ecb00d07d63297
Marcos Cereijo <marcos.rodriguez@pexip.com>
2026-05-15 16:22:14 +0200

    chore: loadConfig before registering the plugin (fail-fast)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { registerPlugin } from '@pexip/plugin-api'
 import { loadConfig } from './loadConfig'
-import type { Config } from './types'
 
 const version = 1
+
+const config = await loadConfig()
 
 const plugin = await registerPlugin({
   id: 'set-clock',
@@ -10,18 +11,6 @@ const plugin = await registerPlugin({
 })
 
 plugin.events.authenticatedWithConference.add(async () => {
-  const config: Config | undefined = await loadConfig().catch(
-    (error: unknown) => {
-      // eslint-disable-next-line no-console -- Log error for failed config load
-      console.error('Failed to load config:', error)
-      return undefined
-    }
-  )
-
-  if (config === undefined) {
-    return
-  }
-
   // Skip if the clock is already configured (result is an object with the current config).
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-type-assertion,@typescript-eslint/no-explicit-any -- Send request is not typed
   const clock = (await (plugin.conference as any).sendRequest({
